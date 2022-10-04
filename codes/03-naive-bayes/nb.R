10f989edd3b87340c4ced758ca9930c952fa1a4f
Eric Lim <5cminsuhlim@gmail.com>
2022-10-04 12:26:16 -0400

    GITHUB-UPLOAD:DATE-2022-10-04-TIME-12-26-15

--- codes/03-naive-bayes/nb.R
+++ codes/03-naive-bayes/nb.R
@@ -3,6 +3,8 @@ library(tidyverse)
 library(e1071)
 library(caTools)
 library(caret)
+library(naivebayes)
+library(klaR)
 
 df <- read.csv('../../data/01-modified-data/big_five_final.csv')
 df <- df %>% dplyr::select(-c(age, case_id, country)) %>%
@@ -11,6 +13,7 @@ df$sex <- as.factor(df$sex)
 
 set.seed(123)
 
+## ref: https://www.learnbymarketing.com/tutorials/naive-bayes-in-r/
 split <- sample.split(df, SplitRatio=0.8)
 train <- subset(df, split=='TRUE')
 test <- subset(df, split=='FALSE')
@@ -18,12 +21,21 @@ test <- subset(df, split=='FALSE')
 classifier <- naiveBayes(sex ~ ., data=train)
 modelPred <- predict(classifier, test)
 confusion <- table(modelPred, test$sex)
-plot(confusion)
 confusionMatrix(confusion)
 
-library(naivebayes)
-library(klaR)
+## ref: https://stackoverflow.com/questions/37897252/plot-confusion-matrix-in-r-using-ggplot
+Target <- factor(c('Male', 'Male', 'Female', 'Female'))
+Prediction <- factor(c(0, 1, 0, 1))
+Y      <- c(26241, 14201, 54263, 7733)
+df <- data.frame(Target, Prediction, Y)
+ggplot(df, aes(x = Target, y = Prediction)) +
+        geom_tile(aes(fill = Y), color = "white") +
+        geom_text(aes(label = sprintf("%1.0f", Y)), vjust = 1) +
+        scale_fill_gradient(low = "blue", high = "red") +
+        scale_x_discrete(labels=c("Female","Male")) +
+        scale_y_discrete(labels=c("Female","Male")) +
+        theme_bw() + 
+        theme(legend.position = "none")
 
 klar <- NaiveBayes(sex ~ ., data = train)
 plot(klar, vars = c("agreeable_score", "extraversion_score", "openness_score", "conscientiousness_score", "neuroticism_score"))
-
