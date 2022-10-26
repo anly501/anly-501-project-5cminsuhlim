34b18f635b1c868aebc90d5008d4c5e9554dd26e
Eric <5cminsuhlim@gmail.com>
2022-10-26 10:23:16 -0400

    GITHUB-UPLOAD:DATE-2022-10-26-TIME-10-23-12

--- codes/04-svm/svm_classification.py
+++ codes/04-svm/svm_classification.py
@@ -5,6 +5,9 @@
 # ### Imports
 
 # %%
+import warnings
+warnings.filterwarnings('ignore')
+
 import sklearn
 from sklearn import datasets
 import numpy as np
@@ -50,7 +53,7 @@ y2=df["sentiment"].to_numpy()
 # INITIALIZE COUNT VECTORIZER
 # minDF = 0.01 means "ignore terms that appear in less than 1% of the documents". 
 # minDF = 5 means "ignore terms that appear in less than 5 documents".
-vectorizer=CountVectorizer(min_df=0.001)   
+vectorizer=CountVectorizer(min_df=0.001,max_features=10000,stop_words="english")   
 
 # RUN COUNT VECTORIZER ON OUR COURPUS 
 Xs  =  vectorizer.fit_transform(corpus)   
@@ -59,10 +62,48 @@ X=np.array(Xs.todense())
 #CONVERT TO ONE-HOT VECTORS
 maxs=np.max(X,axis=0)
 X=np.ceil(X/maxs)
+vocab0 = vectorizer.vocabulary_
 
 # DOUBLE CHECK 
 print(X.shape,y1.shape,y2.shape)
-print("DATA POINT-0:",X[0,0:10],"y1 =",y1[0],"  y2 =",y2[0])
+
+# %%
+#swap keys and values (value --> ley)
+vocab1 = dict([(value, key) for key, value in vocab0.items()])
+
+# %%
+#RE-ORDER COLUMN SO IT IS SORTED FROM HIGH FREQ TERMS TO LOW 
+# https://stackoverflow.com/questions/60758625/sort-pandas-dataframe-by-sum-of-columns
+df2=pd.DataFrame(X)
+s = df2.sum(axis=0)
+df2=df2[s.sort_values(ascending=False).index[:]]
+print(df2.head())
+
+# %%
+# REMAP DICTIONARY TO CORRESPOND TO NEW COLUMN NUMBERS
+print()
+i1=0
+vocab2={}
+for i2 in list(df2.columns):
+    # print(i2)
+    vocab2[i1]=vocab1[int(i2)]
+    i1+=1
+
+#DOUBLE CHECK 
+print(vocab2[0],vocab1[9683])
+print(vocab2[1],vocab1[7785])
+
+
+# %%
+# RENAME COLUMNS 0,1,2,3 .. 
+df2.columns = range(df2.columns.size)
+print(df2.head())
+print(df2.sum(axis=0))
+x=df2.to_numpy()
+
+# %%
+# DOUBLE CHECK 
+print(x.shape,y1.shape)
 
 # %%
 np.unique(y1, return_counts=True)
@@ -88,13 +129,187 @@ random_classifier(y1)
 
 
 # %% [markdown]
+# ### Feature Selection
+
+# %%
+import random
+N=X.shape[0]
+l = [*range(N)]     # indices
+cut = int(0.8 * N) #80% of the list
+random.shuffle(l)   # randomize
+train_index = l[:cut] # first 80% of shuffled list
+test_index = l[cut:] # last 20% of shuffled list
+
+print(train_index[0:10])
+print(test_index[0:10])
+
+# %%
+##UTILITY FUNCTION TO INITIALIZE RELEVANT ARRAYS
+def initialize_arrays():
+    global num_features,train_accuracies
+    global test_accuracies,train_time,eval_time
+    num_features=[]
+    train_accuracies=[]
+    test_accuracies=[]
+    train_time=[]
+    eval_time=[]
+
+# %%
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+import time
+
+def train_SVC_model(X,Y,kernel='linear',i_print=False):
+
+    if(i_print):
+        print(X.shape,Y.shape)
+        print(type(X),type(Y))
+
+    #SPLIT
+    x_train=X[train_index]
+    y_train=Y[train_index].flatten()
+
+    x_test=X[test_index]
+    y_test=Y[test_index].flatten()
+
+    # INITIALIZE MODEL 
+    model = SVC(kernel=kernel)
+
+    # TRAIN MODEL 
+    start = time.process_time()
+    model.fit(x_train,y_train)
+    time_train=time.process_time() - start
+
+    # LABEL PREDICTIONS FOR TRAINING AND TEST SET 
+    start = time.process_time()
+    yp_train = model.predict(x_train)
+    yp_test = model.predict(x_test)
+    time_eval=time.process_time() - start
+
+    acc_train= accuracy_score(y_train, yp_train)*100
+    acc_test= accuracy_score(y_test, yp_test)*100
+
+    if(i_print):
+        print(acc_train,acc_test,time_train,time_eval)
+
+    return (acc_train,acc_test,time_train,time_eval)
+
+# %%
+# DEFINE SEARCH FUNCTION
+def partial_grid_search(num_runs, min_index, max_index):
+    for i in range(1, num_runs+1):
+        # SUBSET FEATURES 
+        upper_index=min_index+i*int((max_index-min_index)/num_runs)
+        xtmp=x[:,0:upper_index]
+
+        #TRAIN 
+        (acc_train,acc_test,time_train,time_eval)=train_SVC_model(xtmp,y1,i_print=False)
+
+        if(i%5==0):
+            print(i,upper_index,xtmp.shape[1],acc_train,acc_test)
+            
+        #RECORD 
+        num_features.append(xtmp.shape[1])
+        train_accuracies.append(acc_train)
+        test_accuracies.append(acc_test)
+        train_time.append(time_train)
+        eval_time.append(time_eval)
+
+# %%
+#UTILITY FUNCTION TO PLOT RESULTS
+def plot_results():
+
+    #PLOT-1
+    plt.plot(num_features,train_accuracies,'-or')
+    plt.plot(num_features,test_accuracies,'-ob')
+    plt.xlabel('Number of features')
+    plt.ylabel('ACCURACY: Training (blue) and Test (red)')
+    plt.show();
+
+    # #PLOT-2
+    plt.plot(num_features,train_time,'-or')
+    plt.plot(num_features,eval_time,'-ob')
+    plt.xlabel('Number of features')
+    plt.ylabel('Runtime: training time (red) and evaluation time(blue)')
+    plt.show();
+
+    # #PLOT-3
+    plt.plot(np.array(test_accuracies),train_time,'-or')
+    plt.plot(np.array(test_accuracies),eval_time,'-ob')
+    plt.xlabel('test_accuracies')
+    plt.ylabel('Runtime: training time (red) and evaluation time (blue)')
+    plt.show();
+
+    # #PLOT-3
+    plt.plot(num_features,np.array(train_accuracies)-np.array(test_accuracies),'-or')
+    plt.xlabel('Number of features')
+    plt.ylabel('train_accuracies-test_accuracies')
+    plt.show();
+
+# %%
+#linear
+(acc_train,acc_test,time_train,time_eval)=train_SVC_model(x,y1,'linear',i_print=True)
+
+initialize_arrays()
+
+# DENSE SEARCH (SMALL NUMBER OF FEATURES (FAST))
+partial_grid_search(num_runs=100, min_index=0, max_index=1000)
+
+# SPARSE SEARCH (LARGE NUMBER OF FEATURES (SLOWER))
+partial_grid_search(num_runs=20, min_index=1000, max_index=10000)
+
+plot_results()
+
+# %%
+#gaussian
+(acc_train,acc_test,time_train,time_eval)=train_SVC_model(x,y1,'rbf',i_print=True)
+
+initialize_arrays()
+
+# DENSE SEARCH (SMALL NUMBER OF FEATURES (FAST))
+partial_grid_search(num_runs=100, min_index=0, max_index=1000)
+
+# SPARSE SEARCH (LARGE NUMBER OF FEATURES (SLOWER))
+partial_grid_search(num_runs=20, min_index=1000, max_index=10000)
+
+plot_results()
+
+# %%
+#sigmoid
+(acc_train,acc_test,time_train,time_eval)=train_SVC_model(x,y1,'sigmoid',i_print=True)
+
+initialize_arrays()
+
+# DENSE SEARCH (SMALL NUMBER OF FEATURES (FAST))
+partial_grid_search(num_runs=100, min_index=0, max_index=1000)
+
+# SPARSE SEARCH (LARGE NUMBER OF FEATURES (SLOWER))
+partial_grid_search(num_runs=20, min_index=1000, max_index=10000)
+
+plot_results()
+
+# %%
+#polynomial
+(acc_train,acc_test,time_train,time_eval)=train_SVC_model(x,y1,'poly',i_print=True)
+
+initialize_arrays()
+
+# DENSE SEARCH (SMALL NUMBER OF FEATURES (FAST))
+partial_grid_search(num_runs=100, min_index=0, max_index=1000)
+
+# SPARSE SEARCH (LARGE NUMBER OF FEATURES (SLOWER))
+partial_grid_search(num_runs=20, min_index=1000, max_index=10000)
+
+plot_results()
+
+# %% [markdown]
 # ### Split Data
 
 # %%
 # PARTITION THE DATASET INTO TRAINING AND TEST SETS
 from sklearn.model_selection import train_test_split
 test_ratio=0.2
-x_train, x_test, y_train, y_test = train_test_split(X, y1, test_size=test_ratio, random_state=0, stratify=y1)
+x_train, x_test, y_train, y_test = train_test_split(X[:, :3000], y1, test_size=test_ratio, random_state=0, stratify=y1)
 y_train=y_train.flatten()
 y_test=y_test.flatten()
 
@@ -106,7 +321,7 @@ print(type(y_test), y_test.shape)
 
 # %%
 accuracy_training_l = []
-accuracy_test_l = []
+accuracy_test_l =  []
 
 # %% [markdown]
 # ### Train Linear Kernel
@@ -281,9 +496,8 @@ ax.set_xticklabels(df['Kernels'])
 from sklearn.model_selection import GridSearchCV
   
 # defining parameter range
-param_grid = {'C': [0.1, 1, 10, 100, 1000], 
-              'gamma': [1, 0.1, 0.01, 0.001, 0.0001],
-              'kernel': ['linear', 'rbf', 'sigmoid', 'polynomial']} 
+param_grid = {'C': [2**-7, 2**-6, 2**-5, 2**-4, 2**-3, 2**-2, 2**-1, 1, 2**1, 2**2, 2**3, 2**4, 2**5, 2**6, 2**7], 
+              'kernel': ['linear']} 
   
 grid = GridSearchCV(SVC(), param_grid, refit = True)
   
@@ -300,7 +514,7 @@ print(grid.best_params_)
 # ### Train Optimal Model
 
 # %%
-model = SVC(kernel='linear', C=0.1, gamma=1)
+model = SVC(kernel=grid.best_params_['kernel'], C=grid.best_params_['C'])
 model = model.fit(x_train, y_train)
 
 yp_train = model.predict(x_train)
